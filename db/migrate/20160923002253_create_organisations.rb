fec44a39b9c61094f340aa8e4845b80a4f1b62eb
assunta09 <assunta.crux@gmail.com>
2016-09-23 14:50:28 -0700

    Add address lines to organisations table --AC

--- db/migrate/20160923002253_create_organisations.rb
+++ db/migrate/20160923002253_create_organisations.rb
@@ -4,6 +4,9 @@ class CreateOrganisations < ActiveRecord::Migration[5.0]
       t.string :name
       t.text :mission
       t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
       t.integer :year_formed
       t.integer :number_of_employees
       t.string :domain
