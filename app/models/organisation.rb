410bf4267584af29ca18fca4b92500a1f26f413d
assunta09 <assunta.crux@gmail.com>
2016-09-23 14:16:32 -0700

    Create impact score --AC

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -5,16 +5,29 @@ class Organisation < ApplicationRecord
   has_many :expenses
 
   def impact_score
-    expenses = Expense.where(organisation_id: self.id).first
-    # total_expenses = expenses.total
-    # admin_expenses = 0
-    # expenses.each do |expense_type, value|
-    #   # if expense_type != 'other'
-    #   #   admin_expenses += value
-    #   # end
-    # end
-
-    # return admin_expenses
+    total_expenses = expense_data.total
+    admin_expenses = 0.00
+    expense_data.attributes.each do |expense_type, value|
+      if expense_type == 'other' || expense_type == 'member_benefits' || expense_type == 'salaries' || expense_type == 'fundraising_fees'
+        if value != nil
+          admin_expenses += value
+        end
+      end
+    end
+    1 - admin_expenses/total_expenses
   end
+
+  def general_expenses_overview
+    expense_data.attributes.each do |expense_type, value|
+
+    end
+  end
+
+
+
+  def expense_data
+    expenses = Expense.where(organisation_id: self.id).first
+  end
+
 
 end
