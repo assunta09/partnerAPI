2e7ecea734f30244afe908ad1fcd6f4b0a33eb04
assunta09 <assunta.crux@gmail.com>
2016-09-23 20:05:28 -0700

    create iteration through files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,59 @@
-Organisation.delete_all
-Revenue.delete_all
-Expense.delete_all
-Executive.delete_all
-Balance.delete_all
+# Organisation.delete_all
+# Revenue.delete_all
+# Expense.delete_all
+# Executive.delete_all
+# Balance.delete_all
 
-org = Organisation.create(
-  name: "Devbootcamp",
-  mission: "Make beautiful and meaningful shit",
-  address: "633 Folsom St.",
-  city: "San Fran",
-  state: "California",
-  zip: 12345,
-  year_formed: 2016,
-  number_of_employees: 1337,
-  domain: "Devbootcamp.com"
-  )
+# org = Organisation.create(
+#   name: "Devbootcamp",
+#   mission: "Make beautiful and meaningful shit",
+#   address: "633 Folsom St.",
+#   city: "San Fran",
+#   state: "California",
+#   zip: 12345,
+#   year_formed: 2016,
+#   number_of_employees: 1337,
+#   domain: "Devbootcamp.com"
+#   )
 
-Revenue.create(
-  organisation_id: org.id,
-  year: 2016,
-  contributions: 5,
-  service_revenue: 5,
-  investments: 5,
-  other: 5,
-  total: 20)
+# Revenue.create(
+#   organisation_id: org.id,
+#   year: 2016,
+#   contributions: 5,
+#   service_revenue: 5,
+#   investments: 5,
+#   other: 5,
+#   total: 20)
 
-Expense.create(
-  organisation_id: org.id,
-  year: 2016,
-  grants: 10,
-  member_benefits: 20,
-  salaries: 20,
-  fundraising_fees: 20,
-  total: 70
-  )
+# Expense.create(
+#   organisation_id: org.id,
+#   year: 2016,
+#   grants: 10,
+#   member_benefits: 20,
+#   salaries: 20,
+#   fundraising_fees: 20,
+#   total: 70
+#   )
 
-Executive.create(
-  organisation_id: org.id,
-  name: "Tedmund Chua",
-  salary: 9999999
-  )
+# Executive.create(
+#   organisation_id: org.id,
+#   name: "Tedmund Chua",
+#   salary: 9999999
+#   )
 
-Balance.create(
-  organisation_id: org.id,
-  year: 2016,
-  total_assets: 500,
-  total_liabilities: 400,
-  net_assets: 100
-  )
+# Balance.create(
+#   organisation_id: org.id,
+#   year: 2016,
+#   total_assets: 500,
+#   total_liabilities: 400,
+#   net_assets: 100
+#   )
 
+
+source_path = Rails.root.join('db', 'xml_files')
+
+Dir.glob("#{source_path}/*.xml").each do |xml_file|
+  puts "This is working"
+end
 
 
