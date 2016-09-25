11887b025f6fa04b0cf347e1b6043955023edf84
Tedmund Chua <email": null>
2016-09-24 18:59:25 -0700

    Delete migration

--- db/migrate/20160925014215_rename_columns.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumns < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :expenses, :grants, :grant_id
-    rename_column :expenses, :other, :other_expense_id
-    rename_column :revenues, :contributions, :contribution_grant_id
-  end
-end
