808da8f495369d185c59c7bc75f7dd9076e06346
Tedmund Chua <email": null>
2016-09-24 19:10:24 -0700

    Remove underscore from tablename

--- db/migrate/20160925020213_create_program_service_accomplishments.rb
+++ db/migrate/20160925020213_create_program_service_accomplishments.rb
@@ -1,6 +1,6 @@
 class CreateProgramServiceAccomplishments < ActiveRecord::Migration[5.0]
   def change
-    create_table :program_service_accomplishments do |t|
+    create_table :programserviceaccomplishments do |t|
       t.integer :organisation_id
       t.integer :expense_amount
       t.integer :grant_amount
