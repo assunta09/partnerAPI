47f7d217958ac6421a009c9b28421b933a45b4ad
Tedmund Chua <email": null>
2016-09-26 11:12:05 -0700

    Reverted back to original table, will change table with migrations instead

--- db/migrate/20160925200433_create_salaries.rb
+++ db/migrate/20160925200433_create_salaries.rb
@@ -2,11 +2,10 @@ class CreateSalaries < ActiveRecord::Migration[5.0]
   def change
     create_table :salaries do |t|
       t.integer :officers_and_key_employees
-      t.integer :comp_to_disqual_persons
       t.integer :general_salaries_and_wages
-      t.integer :pension_plan_accruals
       t.integer :employee_benefits
       t.integer :payroll_taxes
+      t.integer :other_compensation
       t.integer :total
 
       t.timestamps
